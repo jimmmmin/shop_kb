5d3cfe51c3a5e3c6a7aff234cf0db5617cf05037
jimmmmin <wlals20119220@nate.com>
2025-06-13 00:49:22 +0900

    Initial commit

---
+++ src/main/java/com/kb/shop/domain/ShippingInfo.java
@@ -0,0 +1,23 @@
+package com.kb.shop.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class ShippingInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Order id
+    Long orderId;
+    // OrderItem id
+    Long OrderItemId;
+    // shippingStatus
+    private String shippingStatus;
+}
